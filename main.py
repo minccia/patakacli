f3e9d5154226432b903cfc10f0cc6a9a85f8c477
minccia <luisleiteprofissional@gmail.com>
2021-08-03 21:57:16 -0300

    Refactoring

--- main.py
+++
@@ -1,9 +0,0 @@
-from .choicefunction import ChoiceFunction
-
-# Testing
-def dedoNaFulana(nome):
-    print(f'{nome} levou uma dedada')
-
-objeto = ChoiceFunction(dedoNaFulana(), 'Hikari')
-
-print(objeto)
